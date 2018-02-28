052b983eca20c071d26caead198a20c3ee88d38e
student <student@safebear.co.uk>
2018-02-28 16:53:18 +0000

    Changing the app and the pom to match this url's advice: https://www.mkyong.com/spring-boot/spring-boot-deploy-war-file-to-tomcat/

--- src/main/java/com/safebear/springbootwebapp/Application.java
+++ src/main/java/com/safebear/springbootwebapp/Application.java
@@ -11,13 +11,17 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-        return application.sources(applicationClass);
-    }
-    private static Class<Application> applicationClass = Application.class;
+
+
+
 }
