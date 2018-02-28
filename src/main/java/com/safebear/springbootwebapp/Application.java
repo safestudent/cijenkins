eae2cd1ae67fa74bb172fb14ef794a9ffbeb0531
student <student@safebear.co.uk>
2018-02-28 16:41:38 +0000

    Adding the Application code from this website: https://spring.io/blog/2014/03/07/deploying-spring-boot-applications

--- src/main/java/com/safebear/springbootwebapp/Application.java
+++ src/main/java/com/safebear/springbootwebapp/Application.java
@@ -2,14 +2,22 @@ package com.safebear.springbootwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Application main class
  */
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(applicationClass);
+    }
+    private static Class<Application> applicationClass = Application.class;
 }
